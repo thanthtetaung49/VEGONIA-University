4194e7bf96e79c15da07c28c23289ea124d3b5eb
Thant Htet Aung <93987523+thanthtetaung49@users.noreply.github.com>
2022-10-05 08:14:41 +0630

    8:14 am commit

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 // Start JQuery Area 
 $(document).ready(function () {
-  
-  // Start Header 
-  // Start Nav Bar
-  $(".navbuttons").click("click", function() {
-    
-    $(this).toggleClass("crossxs");
-  });
-  // End Nav Bar
-  // End Header
 
-  // Start Login Box
-  $("#open-btn").click(function () {
-    
-    $(".form-popup").css({
+    // Start Header
+    // Start Nav Bar
+    $(".navbuttons").click("click", function () {
 
-      display: "block"
+        $(this).toggleClass("crossxs");
     });
-  });
+    // End Nav Bar
+    // End Header
 
-  $("#formclose-btn").click(function () {
-    
-    $(".form-popup").css({
+    // Start Login Box
+    $("#open-btn").click(function () {
 
-      display: "none"
+        $(".form-popup").css({
+
+            display: "block"
+        });
     });
-  });
-  // End Login Box 
-  
-  
+
+    $("#formclose-btn").click(function () {
+
+        $(".form-popup").css({
+
+            display: "none"
+        });
+    });
+    // End Login Box
+
+
 });
 
 // End JQuery Area
@@ -38,65 +38,66 @@ $(document).ready(function () {
 var getcountervalues = document.querySelectorAll(".countervalues");
 // console.log(getcountervalues);
 
-getcountervalues.forEach(function(getcountervalue) {
-  
-  getcountervalue.textContent = 0;
-  
-  // console.log(getcountervalue);
-  
-  const updatecounter = function() {
-    // console.log("i am working");
-    
-    const countertarget = +getcountervalue.getAttribute("data-target");
-    // console.log(typeof countertarget, countertarget);
-    
-    const getctcontent = +getcountervalue.innerText;
-    // console.log(typeof getctcontent, getctcontent);
-    
-    const increasenumber = countertarget / 100;
-    // console.log(increasenumber);
-    
-    if(getctcontent < countertarget) {
-      
-      getcountervalue.innerText = increasenumber + getctcontent;
-      
-      setTimeout(updatecounter, 100);
+getcountervalues.forEach(function (getcountervalue) {
+
+    getcountervalue.textContent = 0;
+
+    // console.log(getcountervalue);
+
+    const updatecounter = function () {
+        // console.log("i am working");
+
+        const countertarget = +getcountervalue.getAttribute("data-target");
+        // console.log(typeof countertarget, countertarget);
+
+        const getctcontent = +getcountervalue.innerText;
+        // console.log(typeof getctcontent, getctcontent);
+
+        const increasenumber = countertarget / 100;
+        // console.log(increasenumber);
+
+        if (getctcontent < countertarget) {
+
+            getcountervalue.innerText = increasenumber + getctcontent;
+
+            setTimeout(updatecounter, 100);
+        }
     }
-  }
-  
-  updatecounter();
+
+    updatecounter();
 });
 // End Students Counter Section
 
 // Start Rating Section
 // Start google code for chart
-google.charts.load("current", { packages: ["corechart"] });
+google.charts.load("current", {packages: ["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ["Task", "Hours per Day"],
-    ["Myanmar", 8],
-    ["Thailand", 2],
-    ["Signpore", 4],
-    ["Indonesia", 2],
-    ["Sirilanka", 8],
-  ]);
+    var data = google.visualization.arrayToDataTable([
+        ["Task", "Hours per Day"],
+        ["Myanmar", 8],
+        ["Thailand", 2],
+        ["Signpore", 4],
+        ["Indonesia", 2],
+        ["Sirilanka", 8],
+    ]);
 
-  var options = {
-    title: "International Students",
-    // is3D: true,
-    // peHole: 0.4,
-    width: 500,
-    height: 400,
-  };
+    var options = {
+        title: "International Students",
+        // is3D: true,
+        // peHole: 0.4,
+        width: 500,
+        height: 400,
+    };
 
-  var chart = new google.visualization.PieChart(
-    document.getElementById("piechart")
-  );
+    var chart = new google.visualization.PieChart(
+        document.getElementById("piechart")
+    );
 
-  chart.draw(data, options);
+    chart.draw(data, options);
 }
+
 // End google code for chart
 // End Rating Section
 
@@ -106,10 +107,9 @@ const getfullyear = new Date().getUTCFullYear();
 getyear.textContent = getfullyear;
 
 
+// End JavaScript Area
 
-  // End JavaScript Area
-  
-  // 25CT
-  
-  // setTimeout => initial 200 + 200 + 200
-  // setInterval => initial 200 + 200 + 400
+// 25CT
+
+// setTimeout => initial 200 + 200 + 200
+// setInterval => initial 200 + 200 + 400
